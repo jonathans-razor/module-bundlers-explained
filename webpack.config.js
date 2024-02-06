7749ec65657ea8c1f8e561493257347a66a8be8f
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-06 15:53:40 -0500

    Feb-6-2024-3-53-40-PM

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 };
