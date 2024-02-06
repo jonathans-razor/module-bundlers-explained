24ce76c99c359175614475958fa7ae0750b08b32
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-02-06 16:10:13 -0500

    Feb-6-2024-4-10-13-PM

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  entry: "./src/index.js",
+  mode: "development",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       // ... other rules
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
+  },
+  plugins: [new BundleAnalyzerPlugin()],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public")
+    },
   },
 };
